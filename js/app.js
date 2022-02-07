59fe5466e006b34f80bb2e358963dd3709d0d726
Sameer-mishra1 <77093405+Sameer-mishra1@users.noreply.github.com>
2022-02-07 09:21:15 +0530

    First commit

---
+++ js/app.js
@@ -0,0 +1,60 @@
+gsap.registerPlugin(ScrollTrigger)
+
+LottieScrollTrigger({
+	target: "#animation",
+	path: "https://assets3.lottiefiles.com/packages/lf20_8lwqxf4a.json",
+	speed: "slow",
+	scrub: 2, // seconds it takes for the playhead to "catch up"
+	// you can also add ANY ScrollTrigger values here too, like trigger, start, end, onEnter, onLeave, onUpdate, etc. See https://greensock.com/docs/v3/Plugins/ScrollTrigger
+	onLoad() {
+		$('.spinner-wrapper').fadeOut('slow');
+	}
+});
+
+LottieScrollTrigger({
+	target: "#desktopAnimation",
+	path: "assets/desktop2.json",
+	speed: "slow",
+	scrub: 2, // seconds it takes for the playhead to "catch up"
+	// you can also add ANY ScrollTrigger values here too, like trigger, start, end, onEnter, onLeave, onUpdate, etc. See https://greensock.com/docs/v3/Plugins/ScrollTrigger
+	onLoad() {
+		// $('.spinner-wrapper').fadeOut('slow');
+	}
+});
+
+function LottieScrollTrigger(vars) {
+	let playhead = {frame: 0},
+		target = gsap.utils.toArray(vars.target)[0],
+		speeds = {slow: "+=3000", medium: "+=1000", fast: "+=500"},
+		st = {trigger: target, pin: true, start: "top top", end: speeds[vars.speed] || "+=1000", scrub: 1},
+		animation = lottie.loadAnimation({
+			container: target,
+			renderer: vars.renderer || "svg",
+			loop: false,
+			autoplay: false,
+			path: vars.path
+		});
+	for (let p in vars) { // let users override the ScrollTrigger defaults
+		st[p] = vars[p];
+	}
+	animation.addEventListener("DOMLoaded", function() {
+		gsap.to(playhead, {
+			frame: animation.totalFrames - 1,
+			ease: "none",
+			onUpdate: () => animation.goToAndStop(playhead.frame, true),
+			scrollTrigger: st
+		});
+    // in case there are any other ScrollTriggers on the page and the loading of this Lottie asset caused layout changes
+    ScrollTrigger.sort();
+    ScrollTrigger.refresh(); 
+    
+    vars.onLoad() && vars.onLoad();
+	});
+  return animation;
+}
+
+// https://greensock.com/docs/v3/HelperFunctions#lottie
+
+// $(window).load(function() {
+// 	$('.spinner-wrapper').fadeOut('slow');
+// });
---
+++ js/blogs.js
@@ -0,0 +1,39 @@
+gsap.registerPlugin(ScrollTrigger)
+
+LottieScrollTrigger({
+	target: "#reading",
+	path: "assets/blogs.json",
+	speed: "slow",
+	autoplay: true
+})
+
+   function LottieScrollTrigger(vars) {
+	let playhead = {frame: 0},
+		target = gsap.utils.toArray(vars.target)[0],
+		speeds = {slow: "+=3000", medium: "+=1000", fast: "+=500"},
+		st = {trigger: target, pin: true, start: "top top", end: speeds[vars.speed] || "+=1000", scrub: 1},
+		animation = lottie.loadAnimation({
+			container: target,
+			renderer: vars.renderer || "svg",
+			loop: false,
+			autoplay: false,
+			path: vars.path
+		});
+	for (let p in vars) { // let users override the ScrollTrigger defaults
+		st[p] = vars[p];
+	}
+	animation.addEventListener("DOMLoaded", function() {
+		gsap.to(playhead, {
+			frame: animation.totalFrames - 1,
+			ease: "none",
+			onUpdate: () => animation.goToAndStop(playhead.frame, true),
+			scrollTrigger: st
+		});
+    // in case there are any other ScrollTriggers on the page and the loading of this Lottie asset caused layout changes
+    ScrollTrigger.sort();
+    ScrollTrigger.refresh(); 
+	});
+  return animation;
+}
+
+// https://greensock.com/docs/v3/HelperFunctions#lottie
